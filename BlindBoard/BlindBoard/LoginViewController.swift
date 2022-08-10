d902efe708578767868a87f1fbef7734facdadf2
sunshiningsoo <yenkee1219@gmail.com>
2022-08-11 00:41:35 +0900

    Add: Project 만들기

---
+++ BlindBoard/BlindBoard/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  BlindBoard
+//
+//  Created by 박성수 on 2022/08/11.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
