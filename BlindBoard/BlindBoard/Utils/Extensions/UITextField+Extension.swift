d037b4a4d05df322ce992a76e272c5a2ad51ba7e
sunshiningsoo <yenkee1219@gmail.com>
2022-08-12 13:41:07 +0900

    Feat: 로그인뷰 완성

---
+++ BlindBoard/BlindBoard/Utils/Extensions/UITextField+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UITextField+Extension.swift
+//  BlindBoard
+//
+//  Created by 박성수 on 2022/08/12.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField {
+    func addLeftPadding() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 30))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+}
---
+++ BlindBoard/BlindBoard/ViewControllers/LoginViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  BlindBoard
+//
+//  Created by 박성수 on 2022/08/11.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    private let titleLabel: UILabel = {
+        let title = UILabel()
+        title.text = "Blind Board⌨"
+        title.font = .systemFont(ofSize: 28, weight: .bold)
+        return title
+    }()
+    
+    private lazy var idTextfield: UITextField = {
+        let text = UITextField()
+        text.placeholder = "아이디를 입력하세요."
+        text.layer.cornerRadius = 5
+        text.backgroundColor = .lightGray
+        text.addLeftPadding()
+        return text
+    }()
+    
+    private lazy var passwordTextfield: UITextField = {
+        let text = UITextField()
+        text.placeholder = "비밀번호를 입력하세요."
+        text.layer.cornerRadius = 5
+        text.backgroundColor = .lightGray
+        text.addLeftPadding()
+        return text
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("로그인", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    private let registerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("회원가입", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 10
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        render()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        view.backgroundColor = .systemBackground
+    }
+
+    
+    private func render() {
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.topAnchor, paddingTop: 200)
+        titleLabel.centerX(inView: view)
+        
+        view.addSubview(idTextfield)
+        idTextfield.anchor(top: titleLabel.bottomAnchor, paddingTop: 100, width: view.bounds.width - 100, height: 50)
+        idTextfield.centerX(inView: view)
+        
+        view.addSubview(passwordTextfield)
+        passwordTextfield.anchor(top: idTextfield.bottomAnchor, paddingTop: 30, width: view.bounds.width - 100, height: 50)
+        passwordTextfield.centerX(inView: view)
+        
+        view.addSubview(loginButton)
+        loginButton.anchor(top: passwordTextfield.bottomAnchor, paddingTop: 100, width: view.bounds.width - 100, height: 60)
+        loginButton.centerX(inView: view)
+        
+        view.addSubview(registerButton)
+        registerButton.anchor(top: loginButton.bottomAnchor, paddingTop: 30, width: view.bounds.width - 100, height: 60)
+        registerButton.centerX(inView: view)
+    }
+
+}
+
