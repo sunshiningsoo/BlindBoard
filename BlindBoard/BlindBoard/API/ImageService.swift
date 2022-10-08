fed022d956dbbb8b2cdf1131c9e58e8a9ac63d45
sunshiningsoo <yenkee1219@gmail.com>
2022-10-08 22:59:42 +0900

    Refactor: API call change

--- BlindBoard/BlindBoard/API/ImageService.swift
+++ BlindBoard/BlindBoard/API/ImageService.swift
@@ -11,9 +11,9 @@ struct ImageService {
     
     static func imagesFetch(word: String, completion: @escaping(UIImage) -> Void) {
         // TODO: - word에 공백이 있으면 URL ERROR가 남
-        let search = "https://api.pexels.com/v1/search?query=\(word)"
+        let search = "https://api.pexels.com/v1/search?query=\(word)&per_page=3"
         
-        guard let url = URL(string: search) else { return print("URL ERROR")}
+        guard let url = URL(string: search) else { return print("URL ERROR") }
         var request = URLRequest(url: url)
         request.addValue(AUTH.USER_AUTH_KEY, forHTTPHeaderField: "Authorization")
         URLSession(configuration: .default).dataTask(with: request) { data, response, error in
@@ -26,7 +26,7 @@ struct ImageService {
                 guard let imageData = try? JSONDecoder().decode(Img.self, from: data) else { return }
                 guard let photo = imageData.photos else { return }
                 if photo.count != 0 {
-                    guard let photoFirst: ImgInfo? = photo[0] else { return }
+                    guard let photoFirst: ImgInfo? = photo[1] else { return }
                     guard let realurl = URL(string: photoFirst?.src?.original ?? "") else { return print("realURL ERROR") }
                     if let imageReal = try? Data(contentsOf: realurl) {
                         guard let image = UIImage(data: imageReal) else { return }
