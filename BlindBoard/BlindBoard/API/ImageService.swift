dffb67c0c5a7ac888db9ac6b3bdefe21be8d67c3
sunshiningsoo <yenkee1219@gmail.com>
2022-12-02 17:30:47 +0900

    Enhancement: ImageService 예외처리, warning삭제
    - ImgInfo 타입 추론으로 넘김
    - 들어온 String값 white space 제거해줌

--- BlindBoard/BlindBoard/API/ImageService.swift
+++ BlindBoard/BlindBoard/API/ImageService.swift
@@ -10,14 +10,12 @@ import UIKit
 struct ImageService {
     
     static func imagesFetch(word: String, completion: @escaping(UIImage) -> Void) {
-        // TODO: - word에 공백이 있으면 URL ERROR가 남
-        let word = word.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let word = ImageService.eraseWhiteSpace(word)
         let search = "https://api.pexels.com/v1/search?query=\(word)&per_page=3"
-        print(word)
         guard let url = URL(string: search) else { return print("URL ERROR") }
         var request = URLRequest(url: url)
         request.addValue(AUTH.USER_AUTH_KEY, forHTTPHeaderField: "Authorization")
-        URLSession(configuration: .default).dataTask(with: request) { data, response, error in
+        URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data else { return print("DEBUG: DATA ERROR \(String(describing: data?.description))") }
             if let error = error {
                 print("ERROR IN URLSESSION: \(error.localizedDescription)")
@@ -25,8 +23,8 @@ struct ImageService {
             guard let imageData = try? JSONDecoder().decode(Img.self, from: data) else { return }
             guard let photo = imageData.photos else { return }
             if photo.count != 0 {
-                guard let photoFirst: ImgInfo? = photo[1] else { return }
-                guard let realurl = URL(string: photoFirst?.src?.original ?? "") else { return print("realURL ERROR") }
+                guard let photoFirst = photo.first else { return }
+                guard let realurl = URL(string: photoFirst.src?.original ?? "") else { return print("realURL ERROR") }
                 if let imageReal = try? Data(contentsOf: realurl) {
                     guard let image = UIImage(data: imageReal) else { return }
                     completion(image)
@@ -36,5 +34,16 @@ struct ImageService {
             }
         }.resume()
         
+    }
+    
+    static func eraseWhiteSpace(_ word: String) -> String {
+        var temp: String = ""
+        for i in word {
+            if i == " " {
+                continue
+            }
+            temp += String(i)
+        }
+        return temp
     }
 }
